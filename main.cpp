1d5d1ae6cd5ce9378584428c241ae7d88d215fae
Stefano Spinetta <91751558+Diferus@users.noreply.github.com>
2021-12-10 23:15:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include "Lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int dim=0;
+    cin >> dim;
+    if (dim <=0){
+        return 0;
+    }else{
+    float num[dim];
+    for (int i = 0; i < dim; i++) {
+        cin >> num[i];
+        }
+        max_num(num,dim);
+       cout << max_num(num,dim) <<endl;
+    return 0;
+    }
 }
