c290b6622e26abe3ea82f6155e554b8ff2bb7941
Stefano Spinetta <91751558+Diferus@users.noreply.github.com>
2021-12-10 23:15:37 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
+#include "Lib.h"
+float max_num(const float num[],int d){
+    num[d];
+    float max=num[0];
+    for (int i = 0; i < d; i++) {
+        if(num[i]>max){
+            max=num[i];
+        }
+    }
+    return max;
+}
 
